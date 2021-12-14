6335c8070a46d45af360f42faddc566d1973a859
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2021-12-14 09:23:02 -0500

    retrying camtest2

--- Code/camtest2.py
+++ Code/camtest2.py
@@ -6,21 +6,27 @@ with picamera.PiCamera() as camera:
 	camera.resolution = (1024, 786)
 	camera.start_preview()
 	time.sleep(2)
+	
 	print("running")
-	camera.image_effect()
+	
+	camera.image_effects = 'negative'
 	camera.capture('../pics/c1.png')
-	PiCamera.IMAGE_EFFECTS = 'negative'
 	print("image 1")
+	
+	camera.image_effects = 'sketch'
 	camera.capture('../pics/c2.png')
-	PiCamera.IMAGE_EFFECTS = 'sketch'
 	print("image 2")
+	
+	camera.image_effects = 'oilpaint'
 	camera.capture('../pics/c3.png')
-	PiCamera.IMAGE_EFFECTS = 'oilpaint'
 	print("image 3")
+	
+	camera.image_effects = 'blur'
 	camera.capture('../pics/c4.png')
-	PiCamera.IMAGE_EFFECTS = 'blur'
 	print("image 4")
+	
+	camera.image_effects = 'cartoon'
 	camera.capture('../pics/c5.png')
-	PiCamera.IMAGE_EFFECTS = 'cartoon'
 	print("image 5")
+	
 	print("done")
