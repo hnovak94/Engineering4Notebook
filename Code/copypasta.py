b3a4a9f0bf4d29f61dbc3348e71ebcfa67c52ca3
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2022-01-11 09:20:59 -0500

    commented

--- Code/copypasta.py
+++ Code/copypasta.py
@@ -3,16 +3,16 @@ from time import sleep
 from gpiozero import Button
 import time
 
-button = Button(27)
+button = Button(27) # sets pin for button
 
 with picamera.PiCamera() as camera:
-	camera.resolution = (1024, 786)
+	camera.resolution = (1024, 786) # sets resolution of image
 	camera.start_preview()
 	time.sleep(.2)
 
 	print("running")
 
-	button.wait_for_press()
-	camera.capture('../pics/cp.png')
+	button.wait_for_press() 
+	camera.capture('../pics/cp.png') # where image is saved and its name
 	camera.stop_preview
 	print("done")
