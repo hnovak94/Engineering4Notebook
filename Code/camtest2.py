f68e361cf82ef155f122d7044a3e4b0f4f5b2266
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2022-01-11 09:19:35 -0500

    commented

--- Code/camtest2.py
+++ Code/camtest2.py
@@ -3,30 +3,30 @@ import picamera
 
 
 with picamera.PiCamera() as camera:
-	camera.resolution = (1024, 786)
+	camera.resolution = (1024, 786) # sets resolution of picture
 	camera.start_preview()
 	time.sleep(2)
 	
-	print("running")
+	print("running") # so i know the camera is running 
 	
-	camera.image_effect = 'negative'
-	camera.capture('../pics/c1.png')
-	print("image 1")
+	camera.image_effect = 'negative' # applies 'negative' filter
+	camera.capture('../pics/c1.png') # where the image is saved and its name
+	print("image 1") # so i know when image 1 is done, easier to find where the problem is
 	
-	camera.image_effect = 'sketch'
-	camera.capture('../pics/c2.png')
-	print("image 2")
+	camera.image_effect = 'sketch' # applies 'sketch' filter
+	camera.capture('../pics/c2.png') # where the image is saved and its name
+	print("image 2") # i know image 2 is done
 	
-	camera.image_effect = 'oilpaint'
-	camera.capture('../pics/c3.png')
-	print("image 3")
+	camera.image_effect = 'oilpaint' # applies 'oilpaint' filter
+	camera.capture('../pics/c3.png') # where the image is saved and its name
+	print("image 3") # i know image 3 is done
 	
-	camera.image_effect = 'blur'
-	camera.capture('../pics/c4.png')
-	print("image 4")
+	camera.image_effect = 'blur' # applies 'blur' filter
+	camera.capture('../pics/c4.png') # where the image is saved and its name
+	print("image 4") # i know image 4 is done
 	
-	camera.image_effect = 'cartoon'
-	camera.capture('../pics/c5.png')
-	print("image 5")
+	camera.image_effect = 'cartoon' # applies 'cartoon'
+	camera.capture('../pics/c5.png') # where the image is saved and its name
+	print("image 5") # i know image 5 is done
 	
-	print("done")
+	print("done") # done taking all the images 
