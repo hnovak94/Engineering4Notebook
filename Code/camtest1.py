d97243df6893f706e44e810e67c72a06a61c3c2e
hnovak94 <56082415+hnovak94@users.noreply.github.com>
2022-01-11 09:12:55 -0500

    commented

--- Code/camtest1.py
+++ Code/camtest1.py
@@ -2,10 +2,11 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-	camera.resolution = (1024, 768)
+	camera.resolution = (1024, 768) # resolution of the image
 	camera.start_preview()
 	# camera warm up
 	time.sleep(2)
 	print("running")
-	camera.capture('../pics/camtest.png')
+	camera.capture('../pics/camtest.png') # takes picture
+	# () indicates where the image will be saved and what its name will be 
 	print("done")
